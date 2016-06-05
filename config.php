d8944609a84a7c4a064b3b7caacfd4131fc6a069
Daniela Kirsch <daniela.kirsch@helloplugin.de>
2016-06-05 12:33:20 -0400

    up

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+
+
+// $myrootfolder = '/home/dani/';
+// $mydropboxfolder = 'Dropbox/';
+
+$myrootfolder = '/Users/eichnersteve/';
+$mydropboxfolder = 'NamefaceDropbox/Dropbox/';
+
+// look at the file if something changed
+$tasklist = $myrootfolder.$mydropboxfolder.'0cfdatodo/tasklist.txt';
+
+$taskdone = $myrootfolder.$mydropboxfolder.'0cfdatodo/taskdone.txt';
+
+// thumbnailfolder
+$thumbnailfolder = $myrootfolder.$mydropboxfolder.'0cfdathumb/';
+
+// folder for largefiles on local machine
+$locallarge = $myrootfolder.'cfdalargeincoming/';
+
+
+
+// folder for dropboxfiles
+$dropboxlarge = $myrootfolder.$mydropboxfolder.'0cfdalarge/';
+
+
+
+?>
--- remotefileexchange.php
+++ remotefileexchange.php
@@ -1,17 +1,7 @@
 <?php
 
-$myrootfolder = '/home/dani/';
+require 'config.php';
 
-// look at the file if something changed
-$tasklist = $myrootfolder.'Dropbox/0cfdatodo/tasklist.txt';
-
-$taskdone = $myrootfolder.'Dropbox/0cfdatodo/taskdone.txt';
-
-// folder for largefiles on local machine
-$locallarge = $myrootfolder.'cfdalargeincoming/';
-
-// folder for dropboxfiles
-$dropboxlarge = $myrootfolder.'Dropbox/cfdalarge/';
 
 
 
@@ -45,19 +35,22 @@ $todo = array_diff($arraytasks, $arraydone);
 $updatefiledone = "";
 
 for ($i=0; $i < count($todo); $i++) { 
+
+	$myfilename = trim($todo[$i]);
 	
 	// copy file from $locallarge to $dropboxlarge
 	//print_r("nohup cp ".$locallarge.$todo[$i]" ".$dropboxlarge.$todo[$i]" > /dev/null &");
-	shell_exec("nohup cp ".$locallarge.$todo[$i]." ".$dropboxlarge.$todo[$i]." > /dev/null &");
-	shell_exec("nohup chmod 777 ".$dropboxlarge.$todo[$i]." > /dev/null &");
+	shell_exec("nohup cp ".$locallarge.$myfilename." ".$dropboxlarge.$myfilename." > /dev/null &");
+	shell_exec("nohup chmod 777 ".$dropboxlarge.$myfilename." > /dev/null &");
 
-	$updatefiledone .= $todo[$i];
+	
+	$updatefiledone .= $myfilename."\n";
 
 
 }
 
 
-$file = fopen($taskdone,"w");
+$file = fopen($taskdone,"a");
 fwrite($file,$updatefiledone);
 fclose($file);
 
